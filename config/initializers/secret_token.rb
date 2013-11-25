752c6770c1d33d8a22b9b3aa4170c214f2d54cb1
Steven Nunez <snunez@cyrusinnovation.com>
2013-11-25 18:57:22 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReviewShirtsApp::Application.config.secret_key_base = 'a3da0140b22560ed1b88ba8c4e84a09e353e62196a91b22a6237877b36ae832a131a89c22214904a1be026a217b295c03c013382d61c5e5a3a73e7ff95a3885f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReviewShirtsApp::Application.config.session_store :cookie_store, key: '_review_shirts_app_session'
